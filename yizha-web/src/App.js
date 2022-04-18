485a3bbc3187d394eb39e4829b92950b0d3be00a
redarling <dresyv@gmail.com>
2022-04-18 23:06:24 +0300

    Commit test2

--- yizha-web/src/App.js
+++ yizha-web/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">123Rjkz test dev branch</header>
+      <header className="App-header">123Rjnbjkjmkz test dev branch</header>
     </div>
   );
 }
