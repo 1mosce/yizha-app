d3b3839aacfd8d7fae9fa56d4ab95a0b22d13461
1mosce <shuliaknikolay@gmail.com>
2022-05-08 21:50:57 +0300

    Node.js init, Added Login

--- yizha-web/src/App.js
+++ yizha-web/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import Login from "./components/Login/Login";
 
 export default function App() {
+  const [token, setToken] = useState();
+  
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <div>
       <h1>Yizha App - Modern,Fast and Tasty!</h1>
